718d7d51c1a5882210ba88dcd8c0adaa7a05a598
Victoria-Moiseienko <teasevic@gmail.com>
2020-11-11 13:09:36 +0200

    update after review - delimiter was removed, tests were added

--- src/main/java/com/example/springboot/service/impl/FileReaderServiceImpl.java
+++ src/main/java/com/example/springboot/service/impl/FileReaderServiceImpl.java
@@ -5,25 +5,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileReaderServiceImpl implements FileReaderService {
-    private static final String DELIMITER = ",";
 
     @Override
-    public List<List<String>> read(String fileName) {
-        List<List<String>> records = new ArrayList<>();
+    public List<String> read(String fileName) {
+        List<String> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(DELIMITER);
-                records.add(Arrays.asList(values));
+                records.add(line);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("File has not been found", e);
         }
         return records;
     }
--- src/test/java/com/example/springboot/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/com/example/springboot/service/impl/FileReaderServiceImplTest.java
@@ -1,21 +1,31 @@
 package com.example.springboot.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.example.springboot.service.FileReaderService;
-import org.junit.jupiter.api.Test;
-import org.junit.Assert;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.rules.ExpectedException;
 
 class FileReaderServiceImplTest {
+    public final ExpectedException exception = ExpectedException.none();
     private static final String EMPTY_FILE_NAME = "test1";
     private static final String CSV_FILE_NAME = "test2.csv";
+    private static final String CSV_FILE_NAME2 = "test3.csv";
+    private static final String NOT_EXIST_FILE_NAME = "not_exist_file.txt";
     private static final List<List<String>> EMPTY_RESULT = new ArrayList<>();
+    private static final String EXPECTED_RESULT =
+            "Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,HelpfulnessDenominator,Score,Time,Summary,Text";
+    private static final String EXPECTED_RESULT2 =
+            "5,B006K2ZZ7K,A1UQRSCLF8GW1T,\"Michael D. Bigham \"\"M. Wassir\"\"\",0,0,5,1350777600,"
+                   + "Great taffy,\"Great taffy at a great price.  There was a wide assortment of yummy taffy."
+                   + "  Delivery was very quick.  If your a taffy lover, this is a deal.\"";
 
     @Test
     public void readFromEmptyFile() {
         FileReaderService fileReaderService = new FileReaderServiceImpl();
-        List<List<String>> actualResult = fileReaderService.read(EMPTY_FILE_NAME);
+        List<String> actualResult = fileReaderService.read(EMPTY_FILE_NAME);
         Assert.assertEquals("FAILED! Result should be empty",
                 EMPTY_RESULT, actualResult);
     }
@@ -23,13 +33,26 @@ class FileReaderServiceImplTest {
     @Test
     public void readFromCsvFile() {
         FileReaderService fileReaderService = new FileReaderServiceImpl();
-        List<String> expectedResult = List.of("Id", "ProductId", "UserId", "ProfileName",
-                "HelpfulnessNumerator", "HelpfulnessDenominator", "Score", "Time", "Summary", "Text");
-        List<List<String>> actualResult = fileReaderService.read(CSV_FILE_NAME);
-        Assert.assertEquals("FAILED! Size of result should be 3",
+        List<String> actualResult = fileReaderService.read(CSV_FILE_NAME);
+        Assert.assertEquals("FAILED! Unexpected size of result list! ",
                 3, actualResult.size());
-        Assert.assertEquals("FAILED! Size of result should be 9",
-                expectedResult, actualResult.get(0));
+        Assert.assertEquals("FAILED! Unexpected result! ",
+                EXPECTED_RESULT, actualResult.get(0));
     }
 
+    @Test
+    public void readWithoutHeaderFile() {
+        FileReaderService fileReaderService = new FileReaderServiceImpl();
+        List<String> actualResult = fileReaderService.read(CSV_FILE_NAME2);
+        Assert.assertEquals("FAILED! Unexpected result! ",
+                EXPECTED_RESULT2, actualResult.get(0));
+    }
+
+    @Test
+    public void readFromNotExistFile() {
+        FileReaderService fileReaderService = new FileReaderServiceImpl();
+        assertThrows(RuntimeException.class, () -> {
+            fileReaderService.read(NOT_EXIST_FILE_NAME);
+        });
+    }
 }
